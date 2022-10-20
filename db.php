3206208e599e283cb4ffe9a619603936b17c0ebd
Liberti1991 <liberti1991@gmail.com>
2022-10-20 10:06:12 -0300

    auth

--- db.php
+++ db.php
@@ -9,5 +9,4 @@
 
   // Habilitar erros PDO
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);  
-
+  $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
