5ed6aed88170a12c4e8c828952c0ab614dfe2843
Liberti1991 <liberti1991@gmail.com>
2022-10-20 19:13:14 -0300

    fim

--- dashboard.php
+++ dashboard.php
@@ -1,38 +1,38 @@
 <?php
-  require_once("templates/header.php");
+require_once("templates/header.php");
 
-  // Verifica se usuário está autenticado
-  require_once("models/User.php");
-  require_once("dao/UserDAO.php");
-  require_once("dao/MovieDAO.php");
+// Verifica se usuário está autenticado
+require_once("models/User.php");
+require_once("dao/UserDAO.php");
+require_once("dao/MovieDAO.php");
 
-  $user = new User();
-  $userDao = new UserDao($conn, $BASE_URL);
-  $movieDao = new MovieDAO($conn, $BASE_URL);
+$user = new User();
+$userDao = new UserDao($conn, $BASE_URL);
+$movieDao = new MovieDAO($conn, $BASE_URL);
 
-  $userData = $userDao->verifyToken(true);
+$userData = $userDao->verifyToken(true);
 
-  $userMovies = $movieDao->getMoviesByUserId($userData->id);
+$userMovies = $movieDao->getMoviesByUserId($userData->id);
 
 ?>
-  <div id="main-container" class="container-fluid">
-    <h2 class="section-title">Dashboard</h2>
-    <p class="section-description">Adicione ou atualize as informações dos filmes que você enviou</p>
-    <div class="col-md-12" id="add-movie-container">
-      <a href="<?= $BASE_URL ?>newmovie.php" class="btn card-btn">
-        <i class="fas fa-plus"></i> Adicionar Filme
-      </a>
-    </div>
-    <div class="col-md-12" id="movies-dashboard">
-      <table class="table">
-        <thead>
-          <th scope="col">#</th>
-          <th scope="col">Título</th>
-          <th scope="col">Nota</th>
-          <th scope="col" class="actions-column">Ações</th>
-        </thead>
-        <tbody>
-          <?php foreach($userMovies as $movie): ?>
+<div id="main-container" class="container-fluid">
+  <h2 class="section-title">Dashboard</h2>
+  <p class="section-description">Adicione ou atualize as informações dos filmes que você enviou</p>
+  <div class="col-md-12" id="add-movie-container">
+    <a href="<?= $BASE_URL ?>newmovie.php" class="btn card-btn">
+      <i class="fas fa-plus"></i> Adicionar Filme
+    </a>
+  </div>
+  <div class="col-md-12" id="movies-dashboard">
+    <table class="table">
+      <thead>
+        <th scope="col">#</th>
+        <th scope="col">Título</th>
+        <th scope="col">Nota</th>
+        <th scope="col" class="actions-column">Ações</th>
+      </thead>
+      <tbody>
+        <?php foreach ($userMovies as $movie) : ?>
           <tr>
             <td scope="row"><?= $movie->id ?></td>
             <td><a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>" class="table-movie-title"><?= $movie->title ?></a></td>
@@ -50,11 +50,11 @@
               </form>
             </td>
           </tr>
-          <?php endforeach; ?>
-        </tbody>
-      </table>
-    </div>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
   </div>
+</div>
 <?php
-  require_once("templates/footer.php");
+require_once("templates/footer.php");
 ?>
